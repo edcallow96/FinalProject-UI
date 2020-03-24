350a769f1587ab3990f6434c034da3b20419e6ba
ecallow <ed.callow@capgemini.com>
2020-03-24 12:01:23 +0000

    Initial commit

---
+++ src/main/java/com/finalproject/frontend/FrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndApplication {
+
+	public static void main() {
+		SpringApplication.run(FrontEndApplication.class);
+	}
+
+}
